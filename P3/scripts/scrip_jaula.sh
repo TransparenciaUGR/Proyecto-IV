5d67ed60a025d9eb5638356eaad258b2b90a9c8f
Jose Carlos Sánchez Hurtado <josesanz2020@gmail.com>
2014-12-23 18:19:38 +0100

    Update scrip_jaula.sh

--- P3/scripts/scrip_jaula.sh
+++ P3/scripts/scrip_jaula.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Instalar debootstrap, programa para crear jaulas
+# Instalamos debootstrap, programa para crear jaulas
 apt-get install debootstrap
 
-# Crear la jaula
+# Creamos el repositorio donde se almacenaran las jaula
 mkdir /home/jaulas
 
 debootstrap --arch=amd64 trusty /home/jaulas/jaula-iv/ http://archive.ubuntu.com/ubuntu
 
-# Descargar el script de instalación automática
+# Descargamos el script preparar_app.sh para realizar la instalación automática
 wget https://github.com/TransparenciaUGR/Proyecto-IV/blob/master/P3/scripts/preparar_app.sh
 
 # Le damos permisos de ejecución
 chmod +x preparar_app.sh
 
-# Ejecutar la jaula usando el script automático de instalación y ejecución
-# Cuando el script llegue al final tendremos la aplicación funcionando sin tener que tocar nada
-
+# Ejecutamos la jaula usando el script. Una vez hecho esto ya tendremos nuestra aplicación funcionando.
 chroot /home/jaulas/jaula-iv/ < scrip_jaula.sh 
