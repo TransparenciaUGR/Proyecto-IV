385d302c0340dd144ca5b431cb1d4660666e13d5
Alberto Mesa Navarro <AM83@users.noreply.github.com>
2014-12-23 18:16:54 +0100

    script corregido

--- P3/scripts/scrip_jaula.sh
+++ P3/scripts/scrip_jaula.sh
@@ -9,10 +9,10 @@ mkdir /home/jaulas
 debootstrap --arch=amd64 trusty /home/jaulas/jaula-iv/ http://archive.ubuntu.com/ubuntu
 
 # Descargar el script de instalación automática
-wget "-----"
+wget https://github.com/TransparenciaUGR/Proyecto-IV/blob/master/P3/scripts/preparar_app.sh
 
 # Le damos permisos de ejecución
-chmod +x scrip_jaula.sh
+chmod +x preparar_app.sh
 
 # Ejecutar la jaula usando el script automático de instalación y ejecución
 # Cuando el script llegue al final tendremos la aplicación funcionando sin tener que tocar nada
