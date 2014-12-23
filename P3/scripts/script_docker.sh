fcd3526a4f627c3a30fa480d3609dd0f9f6c1d04
Marcos Jiménez Fernández <marcosjf192@gmail.com>
2014-12-23 17:51:27 +0100

    Update script_docker.sh

--- P3/scripts/script_docker.sh
+++ P3/scripts/script_docker.sh
@@ -2,15 +2,15 @@
 #Script de instalación automática de docker.
 
 #En primer lugar actualizamos los repositorios e instalamos docker. Lo hacemos con:
-```
+
 sudo apt-get update
 sudo apt-get install -y docker.io
-```
+
 #Es posible que al ejecutar docker nos de un error con el fichero docker.pid. Para ello, tenemos que eliminarlo de la siguiente forma:
-```
+
 sudo rm /var/run/docker.pid
-```
+
 #Ahora sí, ejecutamos docker:
-```
+
 docker -d &
-```
+
