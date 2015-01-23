accbe816324b4b149dff184f065e5a659d49b256
toranzo <hisahsi90@gmail.com>
2015-01-23 18:53:50 +0100

    Create Gruntfile.js

---
+++ test/scenarios/testFailure/Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+ 
+  grunt.loadTasks('../../../tasks');
+
+  grunt.initConfig({
+    mochaTest: {
+      options: {
+        reporter: 'spec'
+      },
+      all: {
+        src: ['*.js']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['mochaTest']);
+};
