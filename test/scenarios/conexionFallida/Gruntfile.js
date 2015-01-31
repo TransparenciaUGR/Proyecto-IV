aac101b0418cd52cb1be5be9375f228672f41dde
Marcos Jiménez Fernández <marcosjf192@gmail.com>
2015-01-31 19:31:47 +0100

    Create Gruntfile.js

---
+++ test/scenarios/conexionFallida/Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+  grunt.loadTasks('../../../tasks');
+
+  grunt.initConfig({
+    mochaTest: {
+      options: {
+        reporter: 'spec'
+      },
+      all: {
+        src: ['*.js']
+      }
+    }
+  });
+  
+  grunt.registerTask('default', ['mochaTest']);
+};
